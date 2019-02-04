432228fe5a0d019c742e9dc6bc38386670d96217
Hiroki OTA <hiroki.ota@tier4.jp>
2019-02-04 14:09:59 +0900

    feat: add path for cuda to ~/.bashrc

--- install_cuda.sh
+++ install_cuda.sh
@@ -4,3 +4,6 @@ sudo dpkg -i cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt update
 sudo apt install cuda-9-2
+
+echo "export PATH=/usr/local/cuda-9.2/bin:\${PATH}" | tee -a ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-9.2/lib64:\${LD_LIBRARY_PATH}" | tee -a ~/.bashrc
